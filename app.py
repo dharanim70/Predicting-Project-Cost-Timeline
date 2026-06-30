6328387b9720aa2f420e9871a7ada05186880139
Dharani M <2008dharani.m@gmail.com>
2026-06-30 20:07:15 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,47 @@
-from flask import Flask, request
+from flask import Flask, render_template, request
+import joblib
+import pandas as pd
 
 app = Flask(__name__)
 
-@app.route('/')
+# Load trained models
+cost_model = joblib.load("cost_model.pkl")
+timeline_model = joblib.load("timeline_model.pkl")
+
+
+@app.route("/")
 def home():
-    return """
-    <h1>Predicting Project Cost and Timeline</h1>
-    <p>Machine Learning Mini Project</p>
-    """
+    return render_template("index.html")
+
+
+@app.route("/predict", methods=["POST"])
+def predict():
+
+    team_size = int(request.form["team_size"])
+    number_of_tasks = int(request.form["number_of_tasks"])
+    project_complexity = int(request.form["project_complexity"])
+    resource_requirements = int(request.form["resource_requirements"])
+
+    data = pd.DataFrame([[team_size,
+                          number_of_tasks,
+                          project_complexity,
+                          resource_requirements]],
+                        columns=[
+                            "Team_Size",
+                            "Number_of_Tasks",
+                            "Project_Complexity",
+                            "Resource_Requirements"
+                        ])
+
+    predicted_cost = cost_model.predict(data)[0]
+    predicted_timeline = timeline_model.predict(data)[0]
+
+    return render_template(
+        "index.html",
+        predicted_cost=round(predicted_cost, 2),
+        predicted_timeline=round(predicted_timeline, 2)
+    )
+
 
 if __name__ == "__main__":
     app.run(debug=True)
